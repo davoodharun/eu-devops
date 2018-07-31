5e93f6f65159946943a93eb578fe9a338e8a3fff
Ubuntu <davoodharun@davoodharun.mk1ifskgbv0uvakqu2mepfuzle.gx.internal.cloudapp.net>
2018-07-31 00:58:56 +0000

    restructure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -102,6 +102,5 @@ module.exports = {
         // prints more readable module names in the browser console on HMR updates
         new HtmlWebpackPlugin({template: resolve(__dirname, 'src/index.html')}),
         // inject <script> in html file. 
-        new OpenBrowserPlugin({url: 'http://localhost:8080'}),
     ],
 };
