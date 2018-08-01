ec03650f88201987fbc493f303b9fcdfe31d7a34
unknown <davoodharun@gmail.com>
2018-08-01 12:32:19 -0400

    fixes for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,7 +24,7 @@ const app = express();
 
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8080);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
